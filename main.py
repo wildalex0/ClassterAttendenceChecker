48e026aa0d6cffd58d593391743b12b6e7bceb7c
wildalex0 <129412733+wildalex0@users.noreply.github.com>
2024-05-09 17:30:06 +0200

    Back end fully working, api working aswell

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
 import os
 import json
+import datetime
+from math import floor
 UPLOAD_FOLDER = 'TestUpload'
 ALLOWED_EXTENSIONS = {'csv'}
 app = Flask(__name__)
@@ -22,7 +24,6 @@ class Subject():
     def addAttended(self,hours):
         self.attendedLectureHours+= hours    
         self.totalHours += hours
-        print(self.totalHours)
 
     def calcMissedPercentage(self):
         if self.totalHours > 0:
@@ -41,8 +42,12 @@ class Subject():
         else:
             return(False)
     def returnReport(self):
-        return ([self.subjectName, self.calcMissedPercentage()])
-
+        return ([self.subjectName, floor(self.calcMissedPercentage())])
+def writeReport():
+    date = datetime.datetime.now()
+    path = f"AttendencePerDate/{date.day}-{date.month}-{date.year}-{readInstanceCounter()}.json"
+    json.dump(finalJson,open(path,'w'),indent=6)
+    
 def processFile(file):
     path = f"TestUpload/{file}"
     with open(path, 'r') as f:
@@ -59,7 +64,8 @@ def processFile(file):
             if mainList[1] == "Authorised":
                 instanceData['Present'] = True
             
-            nameList = mainList[4].strip().split('|')
+            nameList = mainList[4]
+            nameList = nameList.replace(nameList[0],"",1).split('|')
             if not subjectList:  # Checking if subjectList is empty
                 sub = Subject(nameList[1], nameList[0])
                 subjectList.append(sub)
@@ -75,7 +81,6 @@ def processFile(file):
                         instanceData['Subindex'] = index
                         found = True
                         break
-
                 if not found:  # If the name was not found in subjectList
                     sub = Subject(nameList[1], nameList[0])
                     subjectList.append(sub)
@@ -88,23 +93,32 @@ def processFile(file):
             else:
                 subjectList[instanceData['Subindex']].addMissed(instanceData['HourAmount'])
     for x in subjectList:
-        print(x.returnReport())
+        rp = x.returnReport()
+        finalJson.update({rp[0] : rp[1]})
+    writeReport()    
 def processRow(row):
     counter = 0
     for x in row:
         z = x.replace('"', '')
         counter += 1
         print(f"{counter} - {z}")
-def setInstanceCounter():
+def readInstanceCounter():
     f = open('instanceCounter.txt','r')
     fileCont = int(f.read())
     f.close()
+    return fileCont
+def setInstanceCounter():
+    fileCont = readInstanceCounter()
     print(f"{fileCont} - {type(fileCont)}")
     fileCont+=1 
     fileCont = str(fileCont)
     open('instanceCounter.txt','w').write(fileCont)
     return fileCont
-@app.route('/fileHandle', methods=['GET','POST'])
+@app.route('/fileAPI',methods=['GET','POST'])
+def returnJSON():
+    return finalJson
+
+@app.route('/result', methods=['GET','POST'])
 def mainProcessingAPI():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -116,7 +130,8 @@ def mainProcessingAPI():
             filename = f"{setInstanceCounter()}_{secure_filename(file.filename)}"
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             processFile(filename)
-            return ('Yo')
+
+            return render_template('result.html')
         else:
              return render_template('error.html'), {"Refresh": "3; url=http://localhost:5000/"}
 
