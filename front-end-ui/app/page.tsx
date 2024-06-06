c723f68cd6d707bc11a43760c19f46f855884daf
wildalex0 <129412733+wildalex0@users.noreply.github.com>
2024-06-06 12:28:09 +0200

    started nav bar

--- front-end-ui/app/page.tsx
+++ front-end-ui/app/page.tsx
@@ -1,10 +1,16 @@
-
+import Navbar from "./navbar";
 export default function Home() {
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    
+    <main>
+     
+     <Navbar/>
+
       <div>
         <h1>Test</h1>
       </div>
+
     </main>
   );
 }
