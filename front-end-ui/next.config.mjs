869feab001ee081348e0924ca05619f8a9516d16
wildalex0 <129412733+wildalex0@users.noreply.github.com>
2024-06-14 09:07:12 +0200

    experimenting with direct integration

--- front-end-ui/next.config.mjs
+++ front-end-ui/next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  reactStrictMode: true,
-};
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:5000/api/:path*'
+            : '/api/',
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
