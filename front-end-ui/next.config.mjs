241ad80e865bcd9165185a564477cdb69d7c6ca4
wildalex0 <129412733+wildalex0@users.noreply.github.com>
2024-06-27 11:23:33 +0200

    Revert "experimenting with direct integration"
    This reverts commit 869feab001ee081348e0924ca05619f8a9516d16.

--- front-end-ui/next.config.mjs
+++ front-end-ui/next.config.mjs
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  rewrites: async () => {
-    return [
-      {
-        source: '/api/:path*',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5000/api/:path*'
-            : '/api/',
-      },
-    ]
-  },
-}
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default nextConfig;
--- front-end-ui/src/api/index.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-@app.route("/api/python")
-def hello_world():
-    return("<p>hello world</p>")
